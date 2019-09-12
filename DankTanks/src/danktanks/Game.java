04d44135071b8963d63e5e36fbe224ce210f1bc3
AndrisBorbas <andris.borbas@gmail.com>
2019-09-13 01:09:31 +0200

    add fixed frametiming

--- DankTanks/src/danktanks/Game.java
+++ DankTanks/src/danktanks/Game.java
@@ -54,7 +54,7 @@ public class Game implements Runnable {
 	private void render() {
 		bs = display.getCanvas().getBufferStrategy();
 		if (bs == null) {
-			display.getCanvas().createBufferStrategy(3);
+			display.getCanvas().createBufferStrategy(2);
 			return;
 		}
 		g = bs.getDrawGraphics();
@@ -71,12 +71,52 @@ public class Game implements Runnable {
 
 		init();
 
+		double fps = 60;
+		double timePerTick = 1000000000.0 / fps;
+		double delta = 0;
+		long now;
+		long lastTime = System.nanoTime();
+
+		long timer = 0;
+		int ticks = 0;
+		double frameTime = 0;
+		long frameNow = 1;
+		long frameLast = 1;
+
 		while (running) {
-			tick();
-			render();
+
+			now = System.nanoTime();
+			delta += (now - lastTime) / timePerTick;
+			timer += now - lastTime;
+			lastTime = now;
+
+			if (delta >= 1) {
+
+				frameTime = (frameNow - frameLast) / 1000.0 / 1000.0;
+				frameLast = frameNow;
+				frameNow = System.nanoTime();
+				/*
+				 * System.out.println("FrameTime: " + frameTime);
+				 */
+
+				tick();
+
+				render();
+
+				ticks++;
+				delta--;
+			}
+
+			if (timer >= 1000000000) {
+				/*
+				 * System.out.println("FPS: " + ticks);
+				 */
+				ticks = 0;
+				timer = 0;
+			}
 		}
 
-		// stop();
+		stop();
 	}
 
 }
--- DankTanks/src/danktanks/Startup.java
+++ DankTanks/src/danktanks/Startup.java
@@ -9,23 +9,11 @@ import javax.swing.JFrame;
 public class Startup {
 
 	public static void main(String[] args) {
-		
+
 		Game game = new Game("Dank Tanks", 400, 400);
-		
+
 		game.start();
 
-		/*
-		 * JFrame frame = new JFrame("Dank Tanks"); boolean running = true;
-		 * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); Map canvas = new Map();
-		 * canvas.setSize(400,400); frame.add(canvas); frame.pack();
-		 * frame.setVisible(true);
-		 */
 	}
-
-	/*
-	 * public void run() { while (running) {
-	 * 
-	 * } }
-	 */
 
 }
